16287810d9df7debb61660bd71713712278c9e5a
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-01 23:34:56 +0500

    Task3

---
+++ Tyuiu.AlbornozJ.Sprint1.Task3.V15.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Tyuiu.AlbornozJ.Sprint1.Task3.V15.Lib;
+
+namespace Tyuiu.AlbornozJ.Sprint1.Task3.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double v1 = 60;
+            double v2 = 80;
+            double S = 100;
+            double T = 2;
+            double wait = 380;
+            double res = ds.DistanceOverTime(v1, v2, S, T);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AlbornozJ.Sprint1.Task3.V15/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.AlbornozJ.Sprint1.Task3.V15.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.Write("Введите скорость первого автомобиля V1 (км/ч): ");
+double v1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите скорость второго автомобиля V2 (км/ч): ");
+double v2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите начальное расстояние S (км): ");
+double S = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите время движения T (часов): ");
+double T = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("***********************************");
+Console.WriteLine("* РЕЗУЛЬТАТ: *");
+Console.WriteLine("***********************************");
+
+Console.WriteLine("Расстояние между автомобилями через " + T + " часов = " + ds.DistanceOverTime(v1, v2, S, T) + " км");
+
+Console.ReadKey();
