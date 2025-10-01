0cea94b6cd97cffc50251dd0c93c163047700aa0
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-01 23:07:49 +0500

    Task2

---
+++ Tyuiu.AlbornozJ.Sprint1.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿
+using Tyuiu.AlbornozJ.Sprint1.Task2.V1.Lib;
+
+namespace Tyuiu.AlbornozJ.Sprint1.Task2.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 100;
+            var res = ds.ConvertKmToM(x);
+            Assert.AreEqual(62.150, res);
+        }
+    }
+}
---
+++ Tyuiu.AlbornozJ.Sprint1.Task2.V1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AlbornozJ.Sprint1.Task2.V1.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int x;
+
+Console.WriteLine("Введите расстояние в километрах X:");
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Расстояние в милях Х = " + ds.ConvertKmToM(x).ToString("F3"));
+
+Console.ReadLine();
