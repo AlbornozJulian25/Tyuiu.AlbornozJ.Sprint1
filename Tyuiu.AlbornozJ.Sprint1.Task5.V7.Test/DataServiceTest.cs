95374ce5edcefc7c6e2b4c3f108544aed5f17b39
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-02 12:30:50 +0500

    Task5

---
+++ Tyuiu.AlbornozJ.Sprint1.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Tyuiu.AlbornozJ.Sprint1.Task5.V7.Lib;
+
+namespace Tyuiu.AlbornozJ.Sprint1.Task5.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double f = 180;
+            DataService ds = new DataService();
+            int res = ds.AngleToHoursMinutes(f);
+            int wait = 6;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AlbornozJ.Sprint1.Task5.V7/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.AlbornozJ.Sprint1.Task5.V7.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите угол f (0 < f < 360):");
+double f = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = ds.AngleToHoursMinutes(f);
+Console.WriteLine(res);
+Console.ReadKey();
