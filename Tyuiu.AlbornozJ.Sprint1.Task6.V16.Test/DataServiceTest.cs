8191e9027a150ae492f7d39fa90564de120f2225
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-02 13:03:24 +0500

    Task6

---
+++ Tyuiu.AlbornozJ.Sprint1.Task6.V16.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+using Tyuiu.AlbornozJ.Sprint1.Task6.V16.Lib;
+
+namespace Tyuiu.AlbornozJ.Sprint1.Task6.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            string strTest = "Привет! Как ты?";
+            DataService ds = new DataService();
+            bool res = ds.CheckSpecSymbols(strTest);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AlbornozJ.Sprint1.Task6.V16/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.AlbornozJ.Sprint1.Task6.V16.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите строку:");
+string str = Console.ReadLine();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.CheckSpecSymbols(str));
+
+Console.ReadKey();
